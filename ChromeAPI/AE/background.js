fc72dddc893e77dcd0d80a3228f6f57c77c3eb2f
Michael Odumosu <michaelodumosu57@gmail.com>
2019-07-20 20:36:31 -0400

    learning how to control extension activation

--- ChromeAPI/AE/background.js
+++ ChromeAPI/AE/background.js
@@ -1,15 +1,16 @@
- chrome.runtime.onInstalled.addListener(function() {
-    // chrome.contextMenus.create({
-    //   "id": "sampleContextMenu",
-    //   "title": "Sample Context Menu",
-    //   "contexts": ["selection"]
-    // });
-  });
+//  chrome.runtime.onInstalled.addListener(function() {
+//     // chrome.contextMenus.create({
+//     //   "id": "sampleContextMenu",
+//     //   "title": "Sample Context Menu",
+//     //   "contexts": ["selection"]
+//     // });
+//   });
   
   
     chrome.webNavigation.onCompleted.addListener(function() {
       alert("This is my favorite website!");
-      debugger
+
+      
     chrome.contextMenus.create({
       "id": "sampleContextMenu",
       "title": "Sample Context Menu",
@@ -21,7 +22,33 @@
 /* Add a badge */
         
 
-chrome.browserAction.setBadgeText({text: 'ON'});
-chrome.browserAction.setBadgeBackgroundColor({color: 'red'});
+// chrome.browserAction.setBadgeText({text: 'ON'});
+// chrome.browserAction.setBadgeBackgroundColor({color: 'red'});
 
 /**/
+
+
+/* deciding when an extension can be used  */
+
+
+  chrome.runtime.onInstalled.addListener(function() {
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      // With a new rule ...
+      chrome.declarativeContent.onPageChanged.addRules([
+        {
+          // That fires when a page's URL contains a 'g' ...
+          conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: { urlContains: 'g' },
+            })
+          ],
+          // And shows the extension's page action.
+          actions: [ new chrome.declarativeContent.ShowPageAction() ]
+        }
+      ]);
+    });
+  });
+  
+  
+  /**/
