e1717552818a3a56ec745a96ef2baeaea3d1f86c
Michael Odumosu <michaelodumosu57@gmail.com>
2019-07-20 20:00:19 -0400

    understanding how filter events concepts applies to somethings in the API

--- ChromeAPI/AE/background.js
+++ ChromeAPI/AE/background.js
@@ -1,7 +1,17 @@
  chrome.runtime.onInstalled.addListener(function() {
+    // chrome.contextMenus.create({
+    //   "id": "sampleContextMenu",
+    //   "title": "Sample Context Menu",
+    //   "contexts": ["selection"]
+    // });
+  });
+  
+  
+    chrome.webNavigation.onCompleted.addListener(function() {
+      alert("This is my favorite website!");
     chrome.contextMenus.create({
       "id": "sampleContextMenu",
       "title": "Sample Context Menu",
       "contexts": ["selection"]
     });
-  });
+  }, {url: [{urlMatches : 'https://www.google.com/'}]});
