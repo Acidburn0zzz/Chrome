2af54d72e90ee9ceb48089f1a1f84044321bf911
Michael Odumosu <michaelodumosu57@gmail.com>
2019-07-20 21:42:27 -0400

    learned about how to actually see that contextMenu your looking for

--- ChromeAPI/AE/background.js
+++ ChromeAPI/AE/background.js
@@ -8,7 +8,7 @@
   
   
     chrome.webNavigation.onCompleted.addListener(function() {
-      alert("This is my favorite website!");
+    //   alert("This is my favorite website!");
 
       
     chrome.contextMenus.create({
@@ -31,30 +31,30 @@
 /* deciding when an extension can be used  */
         
 
-  chrome.runtime.onInstalled.addListener(function() {
-    // Replace all rules ...
+//   chrome.runtime.onInstalled.addListener(function() {
+//     // Replace all rules ...
     
 
         
   
   
-    /**/
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      // With a new rule ...
-      chrome.declarativeContent.onPageChanged.addRules([
-        {
-          // That fires when a page's URL contains a 'g' ...
-          conditions: [
-            new chrome.declarativeContent.PageStateMatcher({
-              pageUrl: { urlContains: 'g' },
-            })
-          ],
-          // And shows the extension's page action.
-          actions: [ new chrome.declarativeContent.ShowPageAction() ]
-        }
-      ]);
-    });
-  });
+//     /**/
+//     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+//       // With a new rule ...
+//       chrome.declarativeContent.onPageChanged.addRules([
+//         {
+//           // That fires when a page's URL contains a 'g' ...
+//           conditions: [
+//             new chrome.declarativeContent.PageStateMatcher({
+//               pageUrl: { urlContains: 'g' },
+//             })
+//           ],
+//           // And shows the extension's page action.
+//           actions: [ new chrome.declarativeContent.ShowPageAction() ]
+//         }
+//       ]);
+//     });
+//   });
   
   
   /**/
@@ -64,7 +64,36 @@
 chrome.omnibox.onInputEntered.addListener(function(text) {
   // Encode user input for special characters , / ? : @ & = + $ #
   var newURL = 'https://www.google.com/search?q=' + encodeURIComponent(text);
-  chrome.tabs.create({ url: newURL });
+  chrome.tabs.create({ url: 'https://www.nba.com' });
 });
 
+/**/
+
+
+/*Context Menus*/
+
+  const kLocales = {
+    'com.au': 'Australia',
+    'com.br': 'Brazil',
+    'ca': 'Canada',
+    'cn': 'China',
+    'fr': 'France',
+    'it': 'Italy',
+    'co.in': 'India',
+    'co.jp': 'Japan',
+    'com.ms': 'Mexico',
+    'ru': 'Russia',
+    'co.za': 'South Africa',
+    'co.uk': 'United Kingdom'
+  };
+  chrome.runtime.onInstalled.addListener(function() {
+    for (let key of Object.keys(kLocales)) {
+      chrome.contextMenus.create({
+        id: key,
+        title: kLocales[key],
+        type: 'normal',
+        contexts: ['selection',"page", "image", "link"],
+      });
+    }
+  });
 /**/
